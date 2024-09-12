4c2566e95e66211d0881acab9f16f75f188571c3
Henry Blanchette <henry@monkeyface.com>
2024-09-11 23:38:56 -0400

    init

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,27 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (Aff)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.VDom.Driver as HVD
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = HA.runHalogenAff (HVD.runUI component {} =<< HA.awaitBody)
 
+data Query :: forall k. k -> Type
+data Query a
+
+type Input = {}
+
+data Output
+
+component :: H.Component Query Input Output Aff
+component = H.mkComponent { initialState, eval, render }
+  where
+  initialState _input = {}
+
+  eval = H.mkEval H.defaultEval
+
+  render _state = HH.div [] []
